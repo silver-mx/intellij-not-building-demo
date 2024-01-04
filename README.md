5948fca0040f8871e6c637a6d693fb6555e9a603
Diego Núñez Silva <diego.nunez.mx@gmail.com>
2024-01-04 15:42:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# IntelliJ not building demo
+
+This repo includes a demo application to show how IntelliJ does not build changes
+when the project is open under WSL2 (Windows). 
+
+A screencast of the issue can be seen here: https://drive.google.com/file/d/1XaHv_r5Eqh8ch26YrUsnpakkNhVYWgkY/view?usp=sharing
+
+__NOTE:__ IntelliJ under a Windows environment builds the project correctly, see another screenshot: 
---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("1. Open this project in WSL2 + Intellij (IntelliJ IDEA 2023.3.2 (Ultimate Edition)) ....");
+        System.out.println("2. Make a change in this line and run the file without compiling manually the application...");
+        System.out.println("3. See that the new value is shown in the console...");
+        System.out.println("4. Adding a new line...");
+        System.out.println("5. Another line...");
+    }
+}
